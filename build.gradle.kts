319b2bf668be8c38baa43da6ef21b5cecd2bdc3c
AndreyBogdanov <andrey.bogdanov@cobrainer.com>
2025-07-31 14:41:22 +0200

    Migrate to the new version

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 }
 
 dependencies {
-    implementation("io.github.bay73:generic-ai:0.6.5")
+    implementation("io.github.bay73:generic-ai:0.6.6")
     testImplementation(platform("org.junit:junit-bom:5.13.4"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
